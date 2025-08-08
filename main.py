821d68cf7a00671731839e569efec3d0ecd8724c
Mobin <mobinking18@yahoo.com>
2025-08-09 02:23:12 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import telebot
+import os
+
+# توکن بات رو از متغیر محیطی می‌گیریم
+TOKEN = os.environ.get("8437336707:AAFVvVr5BNBMOsatY_YyGH27-P6kvCwJBvk")
+bot = telebot.TeleBot(TOKEN)
+
+# متن پیام
+MESSAGE = """🟢 Owner $ADA 100X 💲: @Owner_ADA100x_free
+.
+🟢Robot $ADA 100X 💲 : @free100x_ADAbot
+.
+🟢Chanel $ADA 100X 💲 : @ADA100x_free
+.
+🟢Group $ADA 100X 💲: @Company_ADA100x_free
+.
+
+Hello to all $ADA Mining 100x members. As the name of our large and reputable company suggests, we are here to multiply your money by a hundred. ♥️✔️
+
+You may be wondering how this is possible? The answer is that we are a very powerful team of whales and holders of $ADA, and the rise and fall of $ADA's price is in our hands. We are very wealthy (80% of the world's $ADA are in our accounts), and whenever we want, we can lower its price and whenever we like, we can raise its price. Therefore, we can earn a lot of profit from this work and provide you with a hundredfold profit.💲🔈
+"""
+
+# عکس
+PHOTO_PATH = "ada.jpg"
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    with open(PHOTO_PATH, 'rb') as photo:
+        bot.send_photo(message.chat.id, photo, caption=MESSAGE)
+
+print("Bot is running...")
+bot.infinity_polling()
